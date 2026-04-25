07cf5bd29b1d48eda6fabddb292b21e91bfc5c66
seanzhao2021 <93017060+seanzhao2021@users.noreply.github.com>
2026-04-24 19:16:59 -0700

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        #function to merge two lists
+        def merge(head1, head2):
+            prev = ListNode()
+            head = prev
+            
+            while(head1 or head2):
+                #if head1 is None connect the rest to head2
+                if head1 is None:
+                    temp = head2.next
+                    prev.next = head2
+                    prev = prev.next
+                    head2 = temp
+                #if head2 is None connect the rest to head1
+                elif head2 is None:
+                    temp = head1.next
+                    prev.next = head1
+                    prev = prev.next
+                    head1 = temp
+                elif head2.val > head1.val:
+                    temp = head1.next
+                    prev.next = head1
+                    prev = prev.next
+                    head1 = temp
+                else:
+                    temp = head2.next
+                    prev.next = head2
+                    prev = prev.next
+                    head2 = temp
+            
+            return head.next
+
+
+
+        #for i in range len(list) - 1 (3 lists means we run twice)
+        for i in range(len(lists) - 1):
+            #merge(i, i + 1)
+            temp = merge(lists[i], lists[i + 1])
+            #make i + 1 point to new merged list node
+            lists[i + 1] = temp
+
+        
+        if not lists:
+            return None
+        else:
+            return lists[-1]
